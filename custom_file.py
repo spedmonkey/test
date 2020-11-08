68dd89f072c9fd0372e0d6ee344dceccd835b16a
spedmonkey <spedcr@gmail.com>
2020-11-08 20:18:47 +1300

    uyuy

---
+++ custom_file.py
@@ -0,0 +1,14 @@
+class my_class(object):
+    def __init__(self):
+        self.add(2,3)
+
+    def add(self, number1, number2):
+        return (number1 + number2)
+
+    def product(self, number1, number2):
+        return (number1*number2)
+
+    def difference(self, number1, number2):
+        return (number1-number2)
+
+a = my_class()
